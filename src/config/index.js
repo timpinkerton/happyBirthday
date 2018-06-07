ba8b308ab420c8afe816ec9ef10896a2b3c2b0e9
Tim Pinkerton <timpinkerton@mail.com>
2018-06-07 15:26:00 -0400

    updated db config data

--- src/config/index.js
+++ src/config/index.js
@@ -4,8 +4,8 @@ module.exports = {
 
   //including the mongo configuration
   db: {
-    username: 'timpinkerton',
-    password: 'mlabtest1',
+    username: 'Tim',
+    password: 'mLabdbuser1',
     host: 'ds255767.mlab.com:55767',
     dbName: 'fsjs-db1'
   }
